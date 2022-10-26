ab21779d602c785c5a568e4bcec7e592c0f6521c
Doroteya Donova <doroteia.donova@gmail.com>
2022-10-26 12:32:10 +0200

    Did react router dom on App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,42 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+
 import Home from "./pages/Home";
 import Gallery from "./pages/Gallery";
 import Highlights from "./pages/Highlights";
+import About from "./pages/About";
+
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <p>
-        <Home />
-      </p>
-      <p>
-        <Gallery />
-      </p>
-      <p>
-        <Highlights />
-      </p>
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="highlights">Highlights</Link>
+            </li>
+            <li>
+              <Link to="gallery">Gallery</Link>
+            </li>
+            <li>
+              <Link to="about">About</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="highlights" element={<Highlights />} />
+          <Route path="gallery" element={<Gallery />} />
+          <Route path="about" element={<About />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
--- frontend/src/pages/Highlights.jsx
+++ frontend/src/pages/Highlights.jsx
@@ -1,60 +1,60 @@
 import React from "react";
 
 function Highlights() {
-return (
-<div className="highlightsPage">
-<div className="title">
-<h1>Highlights</h1>
-</div>
-<div className="highlightsPictures">
-<img
-src="https://images.unsplash.com/photo-1579783902614-a3fb3927b6a5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1045&q=80"
-alt=""
-/>
-<img
-src="https://images.unsplash.com/flagged/photo-1572392640988-ba48d1a74457?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80"
-alt=""
-/>
-<img
-src="https://images.unsplash.com/photo-1549887552-cb1071d3e5ca?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=765&q=80"
-alt=""
-/>
-<img
-src="https://images.unsplash.com/photo-1536924940846-227afb31e2a5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1166&q=80"
-alt=""
-/>
-<img
-src="https://images.unsplash.com/photo-1499781350541-7783f6c6a0c8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=945&q=80"
-alt=""
-/>
-<img
-src="https://images.unsplash.com/photo-1499781350541-7783f6c6a0c8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=945&q=80"
-alt=""
-/>
-<img
-src="https://images.unsplash.com/photo-1499781350541-7783f6c6a0c8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=945&q=80"
-alt=""
-/>
-<img
-src="https://images.unsplash.com/photo-1499781350541-7783f6c6a0c8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=945&q=80"
-alt=""
-/>
-<img
-src="https://images.unsplash.com/photo-1499781350541-7783f6c6a0c8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=945&q=80"
-alt=""
-/>
-<img
-src="https://images.unsplash.com/photo-1499781350541-7783f6c6a0c8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=945&q=80"
-alt=""
-/>
-</div>
-<div className="artisteDescription">
-<p>Self-Portrait with a Straw Hat (obverse: The Potato Peeler)</p>
-<br />
-<h2 className=".text-center">Vincent van Gogh</h2>
-</div>
-</div>
-);
+  return (
+    <div className="highlightsPage">
+      <div className="title">
+        <h1>Highlights</h1>
+      </div>
+      <div className="highlightsPictures">
+        <img
+          src="https://images.unsplash.com/photo-1579783902614-a3fb3927b6a5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1045&q=80"
+          alt=""
+        />
+        <img
+          src="https://images.unsplash.com/flagged/photo-1572392640988-ba48d1a74457?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80"
+          alt=""
+        />
+        <img
+          src="https://images.unsplash.com/photo-1549887552-cb1071d3e5ca?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=765&q=80"
+          alt=""
+        />
+        <img
+          src="https://images.unsplash.com/photo-1536924940846-227afb31e2a5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1166&q=80"
+          alt=""
+        />
+        <img
+          src="https://images.unsplash.com/photo-1499781350541-7783f6c6a0c8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=945&q=80"
+          alt=""
+        />
+        <img
+          src="https://images.unsplash.com/photo-1499781350541-7783f6c6a0c8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=945&q=80"
+          alt=""
+        />
+        <img
+          src="https://images.unsplash.com/photo-1499781350541-7783f6c6a0c8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=945&q=80"
+          alt=""
+        />
+        <img
+          src="https://images.unsplash.com/photo-1499781350541-7783f6c6a0c8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=945&q=80"
+          alt=""
+        />
+        <img
+          src="https://images.unsplash.com/photo-1499781350541-7783f6c6a0c8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=945&q=80"
+          alt=""
+        />
+        <img
+          src="https://images.unsplash.com/photo-1499781350541-7783f6c6a0c8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=945&q=80"
+          alt=""
+        />
+      </div>
+      <div className="artisteDescription">
+        <p>Self-Portrait with a Straw Hat (obverse: The Potato Peeler)</p>
+        <br />
+        <h2 className=".text-center">Vincent van Gogh</h2>
+      </div>
+    </div>
+  );
 }
 
 export default Highlights;
