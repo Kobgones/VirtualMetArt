26914d41113070b47f0f4b7e13430fa2d2d34fb9
MORGAN MEZAACHE <morgan.mezaache.pro@gmail.comgit config --global user.name MORGAN>
2022-10-26 11:21:27 +0200

    Tailwind Installation

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,16 @@
 import Home from "./pages/Home";
-
+import Gallery from "./pages/Gallery";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <Home />
-      <p>coucou</p>
+      <p>
+        <Home />
+      </p>
+      <p>
+        <Gallery />
+      </p>
     </div>
   );
 }
