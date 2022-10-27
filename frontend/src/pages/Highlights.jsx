0fe33e9634914787585bba18e9a9a54ce58c5839
Davy ROBERT <davyrobert90@gmail.com>
2022-10-27 21:59:26 +0200

    modification Page Highlight

--- frontend/src/pages/Highlights.jsx
+++ frontend/src/pages/Highlights.jsx
@@ -2,11 +2,12 @@ import React from "react";
 
 function Highlights() {
   return (
-    <div className="highlightsPage">
+    <div className="highlightsPage bg-primary text-background">
       <div className="title">
-        <h1>Highlights</h1>
+        <h1 className="text-6xl text-center">Highlights</h1>
       </div>
-      <div className="highlightsPictures">
+      <br />
+      <div className="highlightsPictures flex w-1/4 flex-row">
         <img
           src="https://images.unsplash.com/photo-1579783902614-a3fb3927b6a5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1045&q=80"
           alt=""
@@ -16,7 +17,7 @@ function Highlights() {
           alt=""
         />
         <img
-          src="https://images.unsplash.com/photo-1549887552-cb1071d3e5ca?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=765&q=80"
+          src="https://images.unsplash.com/photo-1499781350541-7783f6c6a0c8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=945&q=80"
           alt=""
         />
         <img
@@ -48,10 +49,11 @@ function Highlights() {
           alt=""
         />
       </div>
-      <div className="artisteDescription">
+      <br />
+      <div className="artisteDescription text-center">
         <p>Self-Portrait with a Straw Hat (obverse: The Potato Peeler)</p>
         <br />
-        <h2 className=".text-center">Vincent van Gogh</h2>
+        <h2 className="text-center text-8xl">Vincent van Gogh</h2>
       </div>
     </div>
   );
