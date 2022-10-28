c8771553966293139103b81463bee15913be55aa
Javier Lopez <jvrlpzd@gmail.com>
2022-10-28 11:04:20 +0200

    Erased content from wrong branch

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-import Footer from "@components/Footer/Footer";
 
 import Home from "./pages/Home";
 import Gallery from "./pages/Gallery";
@@ -11,36 +10,33 @@ import "./styles/Home.css";
 
 function App() {
   return (
-    <div>
-      <Router>
-        <div>
-          <nav>
-            <ul className="flex space-x-4">
-              <li>
-                <Link to="/">Home</Link>
-              </li>
-              <li>
-                <Link to="gallery">Gallery</Link>
-              </li>
-              <li>
-                <Link to="highlights">Highlights</Link>
-              </li>
-              <li>
-                <Link to="about">About</Link>
-              </li>
-            </ul>
-          </nav>
+    <Router>
+      <div>
+        <nav>
+          <ul className="flex space-x-4">
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="gallery">Gallery</Link>
+            </li>
+            <li>
+              <Link to="highlights">Highlights</Link>
+            </li>
+            <li>
+              <Link to="about">About</Link>
+            </li>
+          </ul>
+        </nav>
 
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="gallery" element={<Gallery />} />
-            <Route path="highlights" element={<Highlights />} />
-            <Route path="about" element={<About />} />
-          </Routes>
-        </div>
-      </Router>
-      <Footer />
-    </div>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="gallery" element={<Gallery />} />
+          <Route path="highlights" element={<Highlights />} />
+          <Route path="about" element={<About />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
