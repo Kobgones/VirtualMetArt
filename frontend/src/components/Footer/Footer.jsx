b7c5c39045ac884d4a20fbbfb39af1cd14ec570e
Javier Lopez <jvrlpzd@gmail.com>
2022-11-01 13:29:55 +0100

    Re-made About html structure for better performance and improved responsive design

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -3,15 +3,15 @@ import React from "react";
 function Footer() {
   return (
     // <div className="relative">
-    <div className="footer bg-primary h-16 px-1 flex justify-between items-center">
+    <div className="footer bg-primary h-16 sm:h-20 px-1 flex justify-between items-center relative bottom-0">
       <div className="footer-legal my-auto">
-        <ul className="footer-legal-ul text-white text-xs flex flex-col ml-2">
+        <ul className="footer-legal-ul text-white text-xs flex flex-col ml-2 leading-5 sm:leading-6">
           <li>Conditions générales</li>
           <li>Mentions légales</li>
           <li>2022 All rights reserved</li>
         </ul>
       </div>
-      <div className="footer-socials text-white w-1/3 xl:w-52 mr-2 flex items-center justify-between">
+      <div className="footer-socials text-white w-1/3 sm:w-1/5 xl:w-52 mr-2 flex items-center justify-between">
         <img
           src="../../media/socials/facebook.png"
           className="footer-social-logo w-7 h-7"
--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -4,8 +4,27 @@ import "../styles/About.css";
 function About() {
   return (
     <div className="about-bg bg-background">
-      <div className="bg-[url('./media/about-background.jpg')] bg-cover h-screen flex flex-wrap">
-        <div className="about-title-div w-16 mt-10">
+      <div className="about-image bg-[url('./media/about-background.jpg')] bg-center h-screen flex flex-wrap">
+        <div className="about-title-div w-16 h-0">
+          <h1 className="page-title text-white text-5xl mt-24">About</h1>
+        </div>
+        <div className="about-subtitle w-screen flex flex-wrap justify-center mt-24 pb-4">
+          <p className="text-white text-2xl md:text-3xl min-w-full text-center">
+            About the Met
+          </p>
+          <p className="about-description-p text-justify bg-background mx-8 sm:mx-16 my-8 px-6 md:px-20 lg:px-28 pt-4 sm:pt-10 min-w-xs max-w-4xl">
+            The Metropolitan Museum of Art presents over 5,000 years of art from
+            around the world for everyone to experience and enjoy. The Museum
+            lives in two iconic sites in New York City—The Met Fifth Avenue and
+            The Met Cloisters. Millions of people also take part in The Met
+            experience online. Since its founding in 1870, The Met has always
+            aspired to be more than a treasury of rare and beautiful objects.
+            Every day, art comes alive in the Museum's galleries and through its
+            exhibitions and events, revealing new ideas and unexpected
+            connections across time and across cultures
+          </p>
+        </div>
+        {/* <div className="about-title-div w-16 mt-10">
           <h1 className="page-title text-white text-5xl mt-14 ">About</h1>
         </div>
       </div>
@@ -27,7 +46,7 @@ function About() {
               unexpected connections across time and across cultures
             </p>
           </div>
-        </div>
+        </div> */}
       </div>
     </div>
   );
