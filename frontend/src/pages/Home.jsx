a47a91c2d80608167d912ef240d3940af82896a9
Doroteya Donova <doroteia.donova@gmail.com>
2022-11-01 22:17:50 +0100

    Added media queries for Home page and changed button place

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -4,30 +4,20 @@ import { Link } from "react-router-dom";
 export default function Home() {
   return (
     <div>
-      <div className="bg-[url('./media/home-background-image.jpg')] bg-cover h-screen flex flex-col items-center lg:bg-center">
-        <h1 className="text-7xl text-background text-left drop-shadow-md pt-3 pl-3">
+      <div className="bg-[url('./media/home-background-image.jpg')] bg-cover h-screen flex flex-col items-center lg:bg-[center_70rem]">
+        <h1 className="text-7xl text-background text-left drop-shadow-md pt-3 pl-3 lg:self-start lg:text-8xl lg:w-44 lg:pl-6">
           Virtual Met'Art
         </h1>
-        <Link to="gallery">
-          <button
-            className="bg-gold opacity-90 h-16 w-56 px-4 rounded-md mt-36 lg:w-72 lg:h-20 lg:mt-56"
-            type="button"
-          >
-            <span className="text-primary text-xl font-bold lg:text-2xl">
-              Go to the gallery
-            </span>
-          </button>
-        </Link>
         <div className="down-arrow" />
       </div>
 
-      <section className="bg-background">
+      <section className="bg-background py-6 lg:px-14 lg:py-10 flex flex-col items-center">
         <h2 className="text-primary text-4xl text-center pt-5 px-4 mb-6">
           Start your <span className="text-gold italic">virtual art </span>
           journey with MET !
         </h2>
         <div>
-          <p className="text-primary text-lg text-center pt-6 px-14 pb-6 home-border lg:px-24 lg:text-xl">
+          <p className="text-primary home-border text-lg text-center pt-6 px-14 pb-2 sm:text-2xl lg:px-44">
             Today we take the time to explore the MET from every angle ! Located
             in the heart of New York City, the Metropolitan Museum of Art is one
             of the largest art museums in the world and a must see. You can't
@@ -35,6 +25,16 @@ export default function Home() {
             possibility to explore every MET's exhibition !{" "}
           </p>
         </div>
+        <Link to="gallery">
+          <button
+            className="bg-gold opacity-90 h-16 w-56 px-4 rounded-md mt-10 md:w-64 md:h-20 lg:w-80 lg:h-20 lg:mt-10"
+            type="button"
+          >
+            <span className="text-primary text-xl font-bold md:text-2xl">
+              Go to the gallery
+            </span>
+          </button>
+        </Link>
       </section>
     </div>
   );
