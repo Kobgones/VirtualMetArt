bfcdb85c1faf78fd543adccc341e6ef4a0057517
Davy ROBERT <davyrobert90@gmail.com>
2022-11-03 18:32:57 +0100

    add picture highlights

--- frontend/src/pages/Highlights.jsx
+++ frontend/src/pages/Highlights.jsx
@@ -5,19 +5,21 @@ function Highlights() {
   return (
     <div className="highlightsPage bg-primary text-background">
       <section className="titleAndImages">
-        <div id="titleHighlights ">
-          <h1 className="text-5xl	text-center text-secondary">Highlights</h1>
+        <div id="classTitle ">
+          <h1 className="titleHighlights text-5xl	text-center text-secondary ">
+            Highlights
+          </h1>
         </div>
         <br />
         <div className="carousel">
           <img
             className="imagesCarousel"
-            src="https://france3-regions.francetvinfo.fr/image/tie4hNwjTuy8sa4nPRIyi7qPg6Y/0x0:1417x1049/0x0/regions/2020/06/09/5edef62114e38_autoportrait-en-chapeau-de-feutre-1887_1.jpg"
+            src="./src/assets/vincent van gogh.jpg"
             alt="vincent van gogh"
           />
         </div>
       </section>
-      <br />
+      {/* <br /> */}
       <div className="artisteDescription text-center">
         <p>Self-Portrait with a Straw Hat (obverse: The Potato Peeler)</p>
         <br />
@@ -26,19 +28,46 @@ function Highlights() {
       <div className="classImagesHighlights">
         <img
           className="imagesHighlights"
-          src="https://images.unsplash.com/photo-1579783902614-a3fb3927b6a5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1045&q=80"
+          src="./src/assets/Georgia O’Keeffe.webp"
           alt=""
         />
+        <div className="artisteDescription text-center">
+          <p>Scowhide: Red, white, blue (1931)</p>
+          <br />
+          <h2 className="nameArtist text-center text-6xl">Georgia O’Keeffe</h2>
+        </div>
         <img
           className="imagesHighlights"
-          src="https://images.unsplash.com/flagged/photo-1572392640988-ba48d1a74457?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80"
+          src="./src/assets/Emanuel Leutze.jpeg"
           alt=""
         />
+        <div className="artisteDescription text-center">
+          <p>Washington crossing the Delaware (1851)</p>
+          <br />
+          <h2 className="nameArtist text-center text-6xl">Emanuel Leutze</h2>
+        </div>
         <img
           className="imagesHighlights"
-          src="https://images.unsplash.com/photo-1499781350541-7783f6c6a0c8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=945&q=80"
+          src="./src/assets/Claude-Monet.jpg"
           alt=""
         />
+        <div className="artisteDescription text-center">
+          <p>Bridge over a water lily pond (1899)</p>
+          <br />
+          <h2 className="nameArtist text-center text-6xl">Claude Monet</h2>
+        </div>
+        <img
+          className="imagesHighlights"
+          src="./src/assets/Duccio di Buoninsegna.jpeg"
+          alt=""
+        />
+        <div className="artisteDescription text-center">
+          <p>Virgin and child (1290-1300)</p>
+          <br />
+          <h2 className="nameArtist text-center text-6xl">
+            Duccio di Buoninsegna
+          </h2>
+        </div>
       </div>
       <br />
     </div>
