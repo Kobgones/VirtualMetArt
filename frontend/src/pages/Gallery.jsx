cec0aa887bdb45e6d68ad5d8157c04f8bb8ab131
Javier Lopez <jvrlpzd@gmail.com>
2022-11-09 15:25:17 +0100

    On reste avec cezanne pour l'instant

--- frontend/src/pages/Gallery.jsx
+++ frontend/src/pages/Gallery.jsx
@@ -1,10 +1,19 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Results from "../components/Gallery/Results";
 import Search from "../components/Gallery/Search";
 
 function Gallery() {
   const [ids, setIds] = useState([]);
   const [search, setSearch] = useState("");
+
+  useEffect(() => {
+    fetch(
+      `https://collectionapi.metmuseum.org/public/collection/v1/search?isHighlight=true&hasImages=true&q=cezanne`
+    )
+      .then((response) => response.json())
+      .then((result) => setIds(result.objectIDs))
+      .catch((err) => console.error(err));
+  }, []);
 
   const getSearchIds = () => {
     fetch(
